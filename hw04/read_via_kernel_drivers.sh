dedb3f1bc5ccc1a7c40b10a8ff31364818129a87
David Purdy <Purdydj@Rose-Hulman.edu>
2023-01-10 21:01:29 +0000

    read_via_kernel_drivers.sh

--- hw04/read_via_kernel_drivers.sh
+++ hw04/read_via_kernel_drivers.sh
@@ -5,13 +5,15 @@
 ADDR1=0x49
 ADDR2=0x4a
 
-echo "Ignore resource busy errors, this is expected if the temp sensors are already loaded"
+# "Ignore resource busy errors, this is expected if the temp sensors are already loaded"
+echo "Warning.. Might fail on the first time due to initializing. Rerun again."
+echo ""
 
 # echo "Telling kernel driver to read from tmp101 sensor at address $ADDR1..."
-echo tmp101 $ADDR1 > /sys/class/i2c-adapter/i2c-2/new_device
+echo tmp101 $ADDR1 > /sys/class/i2c-adapter/i2c-2/new_device >> /dev/null
 
 # echo "Telling kernel driver to read from tmp101 sensor at address $ADDR2..."
-echo tmp101 $ADDR2 > /sys/class/i2c-adapter/i2c-2/new_device
+echo tmp101 $ADDR2 > /sys/class/i2c-adapter/i2c-2/new_device >> /dev/null
 
 TEMP1=`cat /sys/bus/i2c/devices/2-0049/hwmon/hwmon0/temp1_input`
 TEMP2=`cat /sys/bus/i2c/devices/2-004a/hwmon/hwmon1/temp1_input`
