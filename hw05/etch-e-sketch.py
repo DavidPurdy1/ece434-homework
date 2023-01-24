ee2708482068e19ded7b009d5f913220ccf22912
David Purdy <Purdydj@Rose-Hulman.edu>
2023-01-24 20:32:17 +0000

    etch sketch finished with acc

--- hw05/etch-e-sketch.py
+++ hw05/etch-e-sketch.py
@@ -95,30 +95,30 @@ while True:
   print(axesx)
 
   # if the accelerometer is tilted more than 1.5g in any direction
-  # if GPIO.input("P8_11") == 1:
-  #   # print("HIGH 11")
-  #   y-=1
-  #   grid[y % yMax][x % xMax] = '*'
-  #   print_grid(grid)
-  #   time.sleep(0.2)
+  if axesy < -0.1:
+    y-=1
+    grid[y % yMax][x % xMax] = '*'
+    print_grid(grid)
+    led_matrix_update(grid)
+    time.sleep(0.2)
 
-  # if GPIO.input("P8_13") == 1:
-  #   # print("HIGH 13")
-  #   y+=1
-  #   grid[y % yMax][x % xMax] = '*'
-  #   print_grid(grid)
-  #   time.sleep(0.2)
+  if axesy > 0.1:
+    y+=1
+    grid[y % yMax][x % xMax] = '*'
+    print_grid(grid)
+    led_matrix_update(grid)
+    time.sleep(0.2)
 
-  # if GPIO.input("P8_19") == 1:
-  #   # print("HIGH 19")
-  #   x-=1
-  #   grid[y % yMax][x % xMax] = '*'
-  #   print_grid(grid)
-  #   time.sleep(0.2)
+  if axesx > 0.1:
+    x-=1
+    grid[y % yMax][x % xMax] = '*'
+    print_grid(grid)
+    led_matrix_update(grid)
+    time.sleep(0.2)
 
-  # if GPIO.input("P8_17") == 1:
-  #   # print("HIGH 17")
-  #   x+=1
-  #   grid[y % yMax][x % xMax] = '*'
-  #   print_grid(grid)
-  #   time.sleep(0.2)
+  if axesx < -0.1:
+    x+=1
+    grid[y % yMax][x % xMax] = '*'
+    print_grid(grid)
+    led_matrix_update(grid)
+    time.sleep(0.2)
